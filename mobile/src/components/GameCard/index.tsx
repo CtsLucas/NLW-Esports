e34d50a63c86781855d6c7c671b29bbd84656cd0
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-09-13 01:32:34 -0300

    feat: layout mobile created

---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,46 @@
+import {
+  TouchableOpacity,
+  ImageBackground,
+  ImageSourcePropType,
+  TouchableOpacityProps,
+  Text,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface CardProps extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: CardProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground 
+        style={styles.cover} 
+        source={data.cover}
+      >
+        <LinearGradient 
+          colors={THEME.COLORS.FOOTER} 
+          style={styles.footer}
+        >
+          <Text style={styles.name}>
+            {data.name}
+          </Text>
+
+          <Text style={styles.ads}>
+            {data.ads} anúncios
+          </Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
